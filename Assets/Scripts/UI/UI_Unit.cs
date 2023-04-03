058db5bf94f29c5658cb5e49451aaca4be24cad9
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-03 22:02:52 +0300

    Update UI_Unit.cs

--- Assets/Scripts/UI/UI_Unit.cs
+++ Assets/Scripts/UI/UI_Unit.cs
@@ -3,9 +3,26 @@ namespace AhmetsHub.ClashOfPirates
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
+    using DevelopersHub.RealtimeNetworking.Client;
+    using TMPro;
 
     public class UI_Unit : MonoBehaviour
     {
-        
+
+        [SerializeField] private Data.UnitID _id = Data.UnitID.pirate;
+        [SerializeField] private Button _button = null;
+        [SerializeField] private TextMeshProUGUI _haveUnitsText = null; 
+
+        private void Start()
+        {
+
+            _button.onClick.AddListener(Clicked);
+        }
+
+        private void Clicked()
+        {
+            
+        }
     }
 }

639ad5e6fc18f6c8d096d818c9ecf357b84e71d6
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-03 22:03:05 +0300

    Create UI_Train.cs

---
+++ Assets/Scripts/UI/UI_Train.cs
@@ -0,0 +1,38 @@
+namespace AhmetsHub.ClashOfPirates
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class UI_Train : MonoBehaviour
+    {
+        
+        [SerializeField] public GameObject _elements = null;
+        [SerializeField] private Button _closeButton = null;
+
+        private static UI_Train _instance = null; public static UI_Train instanse { get { return _instance; } }
+       
+        private void Awake()
+        {
+            _instance = this;
+            _elements.SetActive(false);
+        }
+
+        private void Start()
+        {
+            _closeButton.onClick.AddListener(Close);
+        }
+
+        public void SetStatus(bool status)
+        {
+            _elements.SetActive(status);
+        }
+
+        private void Close()
+        {
+            SetStatus(false);
+            UI_Main.instanse.SetStatus(true);
+        }
+    }
+}

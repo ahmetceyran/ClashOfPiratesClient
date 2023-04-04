c31841747450f6d93b5b714d59f4e01e8327f558
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-05 00:55:23 +0300

    Create UI_UnitsTraining.cs

---
+++ Assets/Scripts/UI/UI_UnitsTraining.cs
@@ -0,0 +1,15 @@
+namespace AhmetsHub.ClashOfPirates
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using TMPro;
+
+    public class UI_UnitsTraining : MonoBehaviour
+    {
+
+        [SerializeField] private TextMeshProUGUI _titleText = null;
+        [SerializeField] private TextMeshProUGUI _countText = null;
+        
+    }
+}

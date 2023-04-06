40f7b38638ed491801b8e80d5e6e59678c362378
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-07 00:33:21 +0300

    Update UI_Unit.cs

--- Assets/Scripts/UI/UI_Unit.cs
+++ Assets/Scripts/UI/UI_Unit.cs
@@ -10,15 +10,37 @@ namespace AhmetsHub.ClashOfPirates
     public class UI_Unit : MonoBehaviour
     {
 
-        [SerializeField] private Data.UnitID _id = Data.UnitID.pirate;
+        [SerializeField] private Data.UnitID _id = Data.UnitID.pirate; public Data.UnitID id { get { return _id; } }
         [SerializeField] private Button _button = null;
-        [SerializeField] private TextMeshProUGUI _haveUnitsText = null; 
+        [SerializeField] private TextMeshProUGUI _haveUnitsText = null;
+        [SerializeField] private TextMeshProUGUI _reqResourceText = null;
+        private int count = 0; public int haveCount { get { return count; } set { count = value; _haveUnitsText.text = count.ToString(); } } 
 
         private void Start()
         {
 
             _button.onClick.AddListener(Clicked);
         }
+
+        public void Initialize(Data.ServerUnit unit)
+    {
+        if (unit.requiredGold > 0)
+        {
+            _reqResourceText.text = "Gold: " + unit.requiredGold.ToString();
+        }
+        else if (unit.requiredFish > 0)
+        {
+            _reqResourceText.text = "Elixir: " + unit.requiredFish.ToString();
+        }
+        else if (unit.requiredDiamonds > 0)
+        {
+            _reqResourceText.text = "Gems: " + unit.requiredDiamonds.ToString();
+        }
+        else
+        {
+            _reqResourceText.text = "Free";
+        }
+    }
 
         private void Clicked()
         {

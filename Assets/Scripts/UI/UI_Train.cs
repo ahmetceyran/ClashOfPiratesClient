68d478231801b733a38540f7f2a15b79c01a411c
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-07 00:33:25 +0300

    Update UI_Train.cs

--- Assets/Scripts/UI/UI_Train.cs
+++ Assets/Scripts/UI/UI_Train.cs
@@ -15,12 +15,30 @@ namespace AhmetsHub.ClashOfPirates
         [SerializeField] private RectTransform _trainGrid = null;
 
         private static UI_Train _instance = null; public static UI_Train instanse { get { return _instance; } }
+
+        private List<UI_UnitsTraining> trainigItems = new List<UI_UnitsTraining>();
+        [SerializeField] private List<UI_Unit> uiUnits = new List<UI_Unit>();
        
         private void Awake()
         {
             _instance = this;
             _elements.SetActive(false);
         }
+
+        public void Initialize()
+    {
+        for (int i = 0; i < uiUnits.Count; i++)
+        {
+            for (int j = 0; j < Player.instanse.initializationData.serverUnits.Count; j++)
+            {
+                if (uiUnits[i].id == Player.instanse.initializationData.serverUnits[j].id)
+                {
+                    uiUnits[i].Initialize(Player.instanse.initializationData.serverUnits[j]);
+                    break;
+                }
+            }
+        }
+    }
 
         private void Start()
         {

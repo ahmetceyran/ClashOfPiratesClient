d6755776490c896cf4deaaadc6937b68c34455f9
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-07 00:33:16 +0300

    Update UI_UnitsTraining.cs

--- Assets/Scripts/UI/UI_UnitsTraining.cs
+++ Assets/Scripts/UI/UI_UnitsTraining.cs
@@ -3,13 +3,58 @@ namespace AhmetsHub.ClashOfPirates
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
     using TMPro;
+    using DevelopersHub.RealtimeNetworking.Client;
 
     public class UI_UnitsTraining : MonoBehaviour
     {
 
         [SerializeField] private TextMeshProUGUI _titleText = null;
-        [SerializeField] private TextMeshProUGUI _countText = null;
+        [SerializeField] private Image _bar = null;
+        [SerializeField] private Button _buttonRemove = null;
+
+        private Data.Unit _unit = null; public long databaseID { get { return _unit != null ? _unit.databaseID : 0; } }
+        [HideInInspector] public Data.UnitID id = Data.UnitID.pirate;
+
+        [HideInInspector] public int index = -1;
+
+        private void Start()
+        {
+            _buttonRemove.onClick.AddListener(Remove);
+        }
+
+        public void Initialize(Data.Unit unit)
+        {
+            _bar.fillAmount = 0;
+            _unit = unit;
+            _titleText.text = _unit.id.ToString();
+        }
+
+        public void Remove()
+        {
+            Packet paket = new Packet();
+            paket.Write((int)Player.RequestsID.CANCELTRAIN);
+            paket.Write(_unit.databaseID);
+            Sender.TCP_Send(paket);
+            //UI_Train.instanse.RemoveTrainingItem(index);
+        }
+
+        private void Update()
+        {
+            if (_unit != null && index == 0)
+            {
+                if (_unit.trainTime <= 0 || _unit.trainedTime >= _unit.trainTime)
+                {
+                    //UI_Train.instanse.RemoveTrainingItem(index);
+                }
+                else
+                {
+                    _unit.trainedTime += Time.deltaTime;
+                    _bar.fillAmount = _unit.trainedTime / _unit.trainTime;
+                }
+            }
+        }
         
     }
 }

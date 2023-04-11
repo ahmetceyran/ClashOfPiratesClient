c1a2335ad97a4aad8b69785bc52388f668216af5
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-04-12 00:41:20 +0300

    Create BattleUnit.cs

---
+++ Assets/Scripts/BattleUnit.cs
@@ -0,0 +1,34 @@
+namespace AhmetsHub.ClashOfPirates
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class BattleUnit : MonoBehaviour
+    {
+
+        public Data.UnitID id = Data.UnitID.pirate;
+        private Vector3 lastPosition = Vector3.zero;
+        private int i = -1; public int index { get { return i; } }
+        private long _id = 0; public long databaseID { get { return _id; } }
+        [HideInInspector] public UI_Bar healthBar = null;
+
+        public void Initialize(int index, long id)
+        {
+            _id = id;
+            i = index;
+            lastPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            if(transform.position != lastPosition)
+            {
+                Vector3 direction = transform.position - lastPosition;
+                lastPosition = transform.position;
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+
+    }
+}

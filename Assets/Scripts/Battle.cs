fa4b2538fe9d2c3af29c5811ada71f16a532209a
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-05-15 22:50:51 +0300

    Update Battle.cs

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -293,7 +293,7 @@ namespace AhmetsHub.ClashOfPirates
                             starCallback.Invoke();
                         }
                     }
-                    int p = (int)Math.Floor(percentage);
+                    int p = (int)Math.Floor(percentage * 100d);
                     if (p >= 50 && !fiftySatar)
                     {
                         fiftySatar = true;

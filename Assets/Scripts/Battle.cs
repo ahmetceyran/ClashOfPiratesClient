e751220f340bf90a629f67d3f80ba7d3ff7cbbde
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-05-25 17:16:52 +0300

    Update Battle.cs

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -150,6 +150,7 @@ namespace AhmetsHub.ClashOfPirates
             public int target = -1;
             public int mainTarget = -1;
             public BattleVector2 position;
+            public BattleVector2 positionOnGrid { get { return new BattleVector2(position.x - Data.battleGridOffset, position.y - Data.battleGridOffset); } }
             public Path path = null;
             public double pathTime = 0;
             public double pathTraveledTime = 0;
@@ -355,8 +356,8 @@ namespace AhmetsHub.ClashOfPirates
             surrender = false;
             this.projectileCallback = projectileCallback;
             _buildings = buildings;
-            grid = new Grid(Data.gridSize, Data.gridSize);
-            unlimitedGrid = new Grid(Data.gridSize, Data.gridSize);
+            grid = new Grid(Data.gridSize + (Data.battleGridOffset * 2), Data.gridSize + (Data.battleGridOffset * 2));
+            unlimitedGrid = new Grid(Data.gridSize + (Data.battleGridOffset * 2), Data.gridSize + (Data.battleGridOffset * 2));
             search = new AStarSearch(grid);
             unlimitedSearch = new AStarSearch(unlimitedGrid);
             for (int i = 0; i < _buildings.Count; i++)
@@ -425,6 +426,9 @@ namespace AhmetsHub.ClashOfPirates
 
         public bool CanAddUnit(int x, int y)
         {
+            x += Data.battleGridOffset;
+            y += Data.battleGridOffset;
+
             for (int i = 0; i < _buildings.Count; i++)
             {
                 if (_buildings[i].health <= 0)
@@ -458,6 +462,8 @@ namespace AhmetsHub.ClashOfPirates
             {
                 return;
             }
+            x += Data.battleGridOffset;
+            y += Data.battleGridOffset;
             UnitToAdd unitToAdd = new UnitToAdd();
             unitToAdd.callback = callback;
             Unit battleUnit = new Unit();
@@ -479,15 +485,19 @@ namespace AhmetsHub.ClashOfPirates
             int addIndex = _units.Count;
             for (int i = _unitsToAdd.Count - 1; i >= 0; i--)
             {
+                /*
                 if (CanAddUnit(_unitsToAdd[i].x, _unitsToAdd[i].y))
                 {
-                    unitsDeployed += _unitsToAdd[i].unit.unit.hosing;
-                    _units.Insert(addIndex, _unitsToAdd[i].unit);
-                    addIndex++;
-                    if (_unitsToAdd[i].callback != null)
-                    {
-                        _unitsToAdd[i].callback.Invoke(_unitsToAdd[i].unit.unit.databaseID);
-                    }
+                    
+                }*/
+                unitsDeployed += _unitsToAdd[i].unit.unit.hosing;
+                _unitsToAdd[i].x += Data.battleGridOffset;
+                _unitsToAdd[i].y += Data.battleGridOffset;
+                _units.Insert(addIndex, _unitsToAdd[i].unit);
+                addIndex++;
+                if (_unitsToAdd[i].callback != null)
+                {
+                    _unitsToAdd[i].callback.Invoke(_unitsToAdd[i].unit.unit.databaseID);
                 }
                 _unitsToAdd.RemoveAt(i);
             }

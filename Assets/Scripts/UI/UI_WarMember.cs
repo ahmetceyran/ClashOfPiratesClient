97e4ee1aaf8faef04658d84483f37bb428089351
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-05-25 17:17:18 +0300

    Update UI_WarMember.cs

--- Assets/Scripts/UI/UI_WarMember.cs
+++ Assets/Scripts/UI/UI_WarMember.cs
@@ -12,8 +12,16 @@ namespace AhmetsHub.ClashOfPirates
         [SerializeField] private Image _image = null;
         [SerializeField] private TextMeshProUGUI _name = null;
         public GameObject selectedEffects = null;
+        [SerializeField] private GameObject bestAttack = null;
+        [SerializeField] private GameObject bestAttackStar1 = null;
+        [SerializeField] private GameObject bestAttackStar2 = null;
+        [SerializeField] private GameObject bestAttackStar3 = null;
+        [SerializeField] private GameObject attack1 = null;
+        [SerializeField] private GameObject attack2 = null;
 
         [HideInInspector] public Data.ClanMember _data = null;
+
+        [HideInInspector] public int remainedAttacks = 0;
 
         private void Start()
         {
@@ -24,11 +32,27 @@ namespace AhmetsHub.ClashOfPirates
             }
         }
 
-        public void Initialize(Data.ClanMember data)
+        public void Initialize(Data.ClanMember data, int attacksDone, Data.ClanWarAttack bestEnemyAttack)
         {
+            remainedAttacks = Data.clanWarAttacksPerPlayer - attacksDone;
             selectedEffects.SetActive(false);
             _data = data;
             _name.text = (_data.warPos + 1).ToString() + ". " + data.name;
+
+            attack1.SetActive(remainedAttacks > 0);
+            attack2.SetActive(remainedAttacks > 1);
+
+            if (bestEnemyAttack != null)
+            {
+                bestAttackStar1.SetActive(bestEnemyAttack.stars > 0);
+                bestAttackStar2.SetActive(bestEnemyAttack.stars > 1);
+                bestAttackStar3.SetActive(bestEnemyAttack.stars > 2);
+                bestAttack.SetActive(true);
+            }
+            else
+            {
+                bestAttack.SetActive(false);
+            }
         }
 
         private void Select()

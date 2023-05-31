b45c82c47d4465a612ed1b52893ca415b2af6e0c
Ahmet Ceyran <ahmetceyran00@gmail.com>
2023-05-31 20:37:57 +0300

    Update UI_BuildingOptions.cs

--- Assets/Scripts/UI/UI_BuildingOptions.cs
+++ Assets/Scripts/UI/UI_BuildingOptions.cs
@@ -17,12 +17,14 @@ namespace AhmetsHub.ClashOfPirates
         public RectTransform instantPanel = null;
         public RectTransform trainPanel = null;
         public RectTransform clanPanel = null;
+        public RectTransform spellPanel = null;
 
         public Button infoButton = null;
         public Button upgradeButton = null;
         public Button instantButton = null;
         public Button trainButton = null;
         public Button clanButton = null;
+        public Button spellButton = null;
 
         private void Awake()
         {
@@ -37,8 +39,9 @@ namespace AhmetsHub.ClashOfPirates
                 infoPanel.gameObject.SetActive(UI_Main.instanse.isActive);
                 upgradePanel.gameObject.SetActive(Building.selectedInstanse.data.isConstructing == false && UI_Main.instanse.isActive);
                 instantPanel.gameObject.SetActive(Building.selectedInstanse.data.isConstructing == true && UI_Main.instanse.isActive);
-                trainPanel.gameObject.SetActive((Building.selectedInstanse.data.id == Data.BuildingID.armycamp || Building.selectedInstanse.data.id == Data.BuildingID.barracks) && UI_Main.instanse.isActive);
-                clanPanel.gameObject.SetActive(Building.selectedInstanse.data.id == Data.BuildingID.clancastle && UI_Main.instanse.isActive);
+                trainPanel.gameObject.SetActive((Building.selectedInstanse.data.id == Data.BuildingID.armycamp || Building.selectedInstanse.data.id == Data.BuildingID.barracks) && UI_Main.instanse.isActive && Building.selectedInstanse.data.level > 0);
+                clanPanel.gameObject.SetActive(Building.selectedInstanse.data.id == Data.BuildingID.clancastle && UI_Main.instanse.isActive && Building.selectedInstanse.data.level > 0);
+                spellPanel.gameObject.SetActive(Building.selectedInstanse.data.id == Data.BuildingID.spellfactory && UI_Main.instanse.isActive && Building.selectedInstanse.data.level > 0);
             }
             _elements.SetActive(status);
         }
